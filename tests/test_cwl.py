596b39fc6f714e30bac3e27e57dbb548a3e79396
Vera Alvarez, Roberto <veraalva@ncbi.nlm.nih.gov>
2018-11-13 14:20:31 -0500

    Fixing test

--- tests/test_cwl.py
+++ tests/test_cwl.py
@@ -8,6 +8,6 @@ class TestSet(unittest.TestCase):
     def test_cwl(self):
         fac = cwltool.factory.Factory()
 
-        echo = fac.make("../tools/basic/echo.cwl")
+        echo = fac.make("./tools/basic/echo.cwl")
         result = echo(out_stdout="echo.stdout",msg="foo")
         self.assertEqual(result['out_stdout']['basename'], 'echo.stdout')
